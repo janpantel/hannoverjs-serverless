657a4d55cef191eae163bb102c8db1df6547c02b
Jan-Oliver Pantel <jpantel@newstore.com>
2016-11-21 13:36:49 +0100

    add createMeetup function

--- handler.js
+++ handler.js
@@ -1,16 +1,30 @@
-'use strict';
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.createMeetup = (event, context, callback) => {
+    const data = JSON.parse(event.body) || {};
+    var meetup = {
+        id: uuid()
+    };
 
-  callback(null, response);
+    if (!data.name) {
+        return callback(null, { statusCode: 400, body: 'Missing meetup name' });
+    }
+    meetup.name = data.name;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    if (!data.date) {
+        return callback(null, { statusCode: 400, body: 'Missing event date' });
+    } else if (isNaN(Date.parse(data.date))) {
+        return callback(null, { statusCode: 400, body: 'No valid date given' });
+    }
+    meetup.date = data.date;
+
+    dynamo.put({ TableName: "meetups", Item: meetup }, (err) => {
+        if (err) {
+            console.log("error persisting meetup: ", err);
+            return callback(err);
+        }
+        return callback(null, { statusCode: 200, body: JSON.stringify(meetup) });
+    });
 };
