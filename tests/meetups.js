109538ed77fa0caff19e792a4481cc68f77a34b8
Jan-Oliver Pantel <jpantel@newstore.com>
2016-11-23 14:57:50 +0100

    add tests for meetup list retrieval

--- tests/meetups.js
+++ tests/meetups.js
@@ -35,3 +35,24 @@ test.cb('meetups can be created', t => {
         t.end();
     });
 });
+
+test.cb('meetups list can be retrieved', t => {
+    const meetup = {
+        name: "hannover js",
+        date: "2017-01-01T00:00:00.000Z"
+    };
+    fetch(tools.apiUrl('/meetup'), { method: 'POST', payload: JSON.stringify(meetup) }, (err, meta, body) => {
+        t.ifError(err, "error executing meetup creation");
+        t.is(meta.status, 200, body);
+
+        fetch(tools.apiUrl('/meetup'), (err, meta, body) => {
+            t.ifError(err, 'error retrieving meetup list');
+            t.is(meta.status, 200);
+
+            const payload = JSON.parse(body) || {};
+            t.truthy(payload.items.length, 'not enough items');
+
+            t.end();
+        });
+    });
+});
